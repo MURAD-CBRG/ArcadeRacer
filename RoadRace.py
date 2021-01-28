b55761342d695d9613be845098198784ddc348d9
MURAD-CBRG <murad.efendiev05@yandex.ru>
2021-01-28 22:42:38 +0300

    ArcadeRacer v1.1

--- RoadRace.py
+++ RoadRace.py
@@ -21,6 +21,12 @@ class Shop:
         self.bg = load_image('main_menu_bg.jpg', SCREEN_SIZE)
         self.font = pygame.font.Font('data/fonts/impact.ttf', 50)
 
+        money_base = open('money_count.txt', 'r', encoding='utf-8')
+        money_information = money_base.readlines()[0].strip()
+        money_base.close()
+
+        self.money_text = self.font.render(money_information + '$', True, pygame.Color('white'))
+
         self.manager = pygame_gui.UIManager((700, 600))
 
         self.main_menu_btn = pygame_gui.elements.UIButton(
@@ -30,24 +36,43 @@ class Shop:
         )
 
         self.car1_btn = pygame_gui.elements.UIButton(
-            relative_rect=pygame.Rect((50, 70), (190, 490)),
+            relative_rect=pygame.Rect((50, 70), (190, 190)),
             text='Car1 (Standart)',
             manager=self.manager
         )
 
         self.car2_btn = pygame_gui.elements.UIButton(
-            relative_rect=pygame.Rect((250, 70), (190, 490)),
+            relative_rect=pygame.Rect((250, 70), (190, 190)),
             text='Car2 (Cost: 1000$)',
             manager=self.manager
         )
 
         self.car3_btn = pygame_gui.elements.UIButton(
-            relative_rect=pygame.Rect((450, 70), (190, 490)),
+            relative_rect=pygame.Rect((450, 70), (190, 190)),
             text='Car3 (Cost: 10000$)',
             manager=self.manager
         )
 
+        self.road1_btn = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect((50, 280), (190, 190)),
+            text='Road1 (Standart)',
+            manager=self.manager
+        )
+
+        self.road2_btn = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect((250, 280), (190, 190)),
+            text='Road2 (Cost: 1000$)',
+            manager=self.manager
+        )
+
+        self.road3_btn = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect((450, 280), (190, 190)),
+            text='Road3 (Cost: 10000$)',
+            manager=self.manager
+        )
+
         screen.blit(self.bg, (0, 0))
+        screen.blit(self.money_text, (350, 10))
 
 
 class Info:
@@ -86,7 +111,7 @@ class Game:
             car_size = [120, 150]
 
         self.player = pygame.sprite.Sprite()
-        self.player.image = load_image(sprite_name, [90, 130])
+        self.player.image = load_image(sprite_name, car_size)
         self.player.rect = self.player.image.get_rect()
         self.player.rect.x, self.player.rect.y = 300, 300
         self.player.mask = pygame.mask.from_surface(self.player.image)
@@ -94,14 +119,22 @@ class Game:
         self.is_move = [False, False]
 
         self.box_base = list()
+        self.money_base = list()
 
         self.all_sprites.add(self.player)
 
-        self.bg = load_image('road.jpg', SCREEN_SIZE)
+        reader = open('bg.txt', 'r', encoding='utf-8')
+        info_reader = reader.readlines()[0].strip()
+        reader.close()
+
+        self.bg = load_image(info_reader, SCREEN_SIZE)
 
         screen.blit(self.bg, (0, 0))
 
-        self.render_counter, self.render_speed, self.speed_x, self.speed_y = int(), 150, 3, 1
+        self.render_counter = int()
+        self.render_speed = 150
+        self.speed_x = 3
+        self.speed_y = 1
 
         if sprite_name == 'car2.png':
             self.speed_x = 4
@@ -114,18 +147,31 @@ class Game:
         self.render_counter += 1
 
         if self.render_counter >= self.render_speed:
-            self.box_base.append(pygame.sprite.Sprite())
             enemy_sprite = random.choice(['enemy1.jpg', 'box1.jpg'])
 
-            if random.randint(0, 10) == 6:
-                self.box_base[-1].image = load_image('money.jpg', [30, 30])
-            else:
-                self.box_base[-1].image = load_image(enemy_sprite, [140, 120])
+            if random.randint(0, 10) > 6:
+                if len(self.money_base) == 0:
+                    self.money_base.append(pygame.sprite.Sprite())
+                    self.money_base[0].image = load_image('money.jpg', [30, 30])
+                else:
+                    self.money_base[-1].image = load_image('money.jpg', [30, 30])
 
-            self.box_base[-1].rect = self.box_base[-1].image.get_rect()
-            self.box_base[-1].rect.x, self.box_base[-1].rect.y = random.randint(20, 590), random.randint(0, 0)
-            self.box_base[-1].mask = pygame.mask.from_surface(self.box_base[-1].image)
-            self.all_sprites.add(self.box_base[-1])
+                self.money_base[-1].rect = self.money_base[-1].image.get_rect()
+                self.money_base[-1].rect.x, self.money_base[-1].rect.y = random.randint(20, 590), random.randint(0, 0)
+                self.money_base[-1].mask = pygame.mask.from_surface(self.money_base[-1].image)
+                self.all_sprites.add(self.money_base[-1])
+            else:
+                if len(self.box_base) == 0:
+                    self.box_base.append(pygame.sprite.Sprite())
+                    self.box_base[0].image = load_image(enemy_sprite, [140, 120])
+                else:
+                    self.box_base[-1].image = load_image(enemy_sprite, [140, 120])
+
+                self.box_base[-1].rect = self.box_base[-1].image.get_rect()
+                self.box_base[-1].rect.x, self.box_base[-1].rect.y = random.randint(20, 590), random.randint(0, 0)
+                self.box_base[-1].mask = pygame.mask.from_surface(self.box_base[-1].image)
+                self.all_sprites.add(self.box_base[-1])
+
             self.render_counter = int()
 
             if self.render_speed > 60:
@@ -137,20 +183,23 @@ class Game:
 
         self.score += 1
 
-        for i in range(len(self.box_base)):
-            if pygame.sprite.collide_mask(self.player, self.box_base[i]) and self.box_base[i].sprite in \
-                    ('enemy1.jpg', 'box1.jpg'):
-                flag = True
-            elif pygame.sprite.collide_mask(self.player, self.box_base[i]) and self.box_base[i].sprite == 'money.jpg':
+        for i in range(len(self.money_base)):
+            if pygame.sprite.collide_mask(self.player, self.money_base[i]):
                 reader = open('money_count.txt', 'r', encoding='utf-8')
-                writer = open('money_count.txt', 'w', encoding='utf-8')
-
-                reader = int(reader.readlines()[0].strip())
-
-                writer.write(str(reader + 100))
-
+                reader_info = int(reader.readlines()[0].strip())
                 reader.close()
+
+                writer = open('money_count.txt', 'w', encoding='utf-8')
+                writer.write(str(reader_info + 100))
                 writer.close()
+
+                self.money_base[i].rect.y += 10 ** 3
+
+            self.money_base[i].rect.y += self.speed_y
+
+        for i in range(len(self.box_base)):
+            if pygame.sprite.collide_mask(self.player, self.box_base[i]):
+                flag = True
 
             self.box_base[i].rect.y += self.speed_y
 
@@ -214,15 +263,89 @@ def loader_img(sprite_name, cost):
 
     font = pygame.font.Font('data/fonts/impact.ttf', 22)
 
-    if information >= cost:
-        writer = open('player_sprite.txt', 'w', encoding='utf-8')
-        sprite_name = writer.write(sprite_name)
-        writer.close()
-        text = font.render("Bought have been success", True, pygame.Color('white'))
-    else:
-        text = font.render("You can't bought the car", True, pygame.Color('white'))
+    flag = True
 
-    screen.blit(text, [20, 570])
+    data_base = open('car_base.txt', 'r', encoding='utf-8')
+    info_data_base = data_base.readlines()
+
+    for i in info_data_base:
+        if i.strip() == sprite_name:
+            writer = open('player_sprite.txt', 'w', encoding='utf-8')
+            writer.write(sprite_name)
+            flag = False
+
+    data_base.close()
+
+    if flag:
+        if information >= cost:
+            writer = open('player_sprite.txt', 'w', encoding='utf-8')
+            writer.write(sprite_name)
+            writer.close()
+
+            reader = open('money_count.txt', 'r', encoding='utf-8')
+            info_reader = int(reader.readlines()[0].strip())
+            reader.close()
+
+            writer_money = open('money_count.txt', 'w', encoding='utf-8')
+            writer_money.write(str(info_reader - cost))
+            writer_money.close()
+
+            text = font.render("Bought have been success", True, pygame.Color('white'))
+
+            appender_file = open('car_base.txt', 'r', encoding='utf-8')
+            appender_file.write(sprite_name + '\n')
+            appender_file.close()
+        else:
+            text = font.render("You can't bought the car", True, pygame.Color('white'))
+
+        screen.blit(text, [20, 570])
+
+    reader.close()
+
+
+def loader_img_road(sprite_name, cost):
+    reader = open('money_count.txt', 'r', encoding='utf-8')
+
+    information = int(reader.readlines()[0].strip())
+
+    font = pygame.font.Font('data/fonts/impact.ttf', 22)
+
+    flag = True
+
+    data_base = open('road_base.txt', 'r', encoding='utf-8')
+    info_data_base = data_base.readlines()
+
+    for i in info_data_base:
+        if i.strip() == sprite_name:
+            writer = open('bg.txt', 'w', encoding='utf-8')
+            writer.write(sprite_name)
+            flag = False
+
+    data_base.close()
+
+    if flag:
+        if information >= cost:
+            writer = open('bg.txt', 'w', encoding='utf-8')
+            writer.write(sprite_name)
+            writer.close()
+
+            reader = open('money_count.txt', 'r', encoding='utf-8')
+            info_reader = int(reader.readlines()[0].strip())
+            reader.close()
+
+            writer_money = open('money_count.txt', 'w', encoding='utf-8')
+            writer_money.write(str(info_reader - cost))
+            writer_money.close()
+
+            text = font.render("Bought have been success", True, pygame.Color('white'))
+
+            appender_file = open('road_base.txt', 'r', encoding='utf-8')
+            appender_file.write(sprite_name + '\n')
+            appender_file.close()
+        else:
+            text = font.render("You can't bought the road", True, pygame.Color('white'))
+
+        screen.blit(text, [20, 570])
 
     reader.close()
 
@@ -334,7 +457,12 @@ if __name__ == '__main__':
                             loader_img('car2.jpg', 5000)
                         elif i.ui_element == scene.car3_btn:
                             loader_img('car3.jpg', 10000)
-
+                        elif i.ui_element == scene.road1_btn:
+                            loader_img('road4.jpg', 0)
+                        elif i.ui_element == scene.road2_btn:
+                            loader_img('road2.jpg', 5000)
+                        elif i.ui_element == scene.road3_btn:
+                            loader_img('road.jpg', 10000)
             if controller == 0:
                 manager.process_events(i)
             elif controller in (3, 4):
